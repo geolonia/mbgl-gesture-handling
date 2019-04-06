29fc33de6179541cb63f7cd8f03ac00d0f400c8b
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-06 10:14:25 +0900

    some fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 "use strict"
 
+let timer;
+
 class GestureHandling {
   constructor(options) {
     const defaults = {
       backgroundColor: 'rgba(0, 0, 0, 0.8)',
       textColor: '#ffffff',
-      textMessage: 'Use [alt] + scroll to zoom the map.',
+      textMessage: 'Use alt + scroll to zoom the map.',
       timeout: 2000,
     };
 
@@ -36,19 +38,21 @@ class GestureHandling {
     const container = map.getContainer();
     const rect = container.getBoundingClientRect();
 
-    this.alertBox.style.top = `${rect.top}px`;
-    this.alertBox.style.left = `${rect.left}px`;
+    this.alertBox.style.top = `${rect.top + window.scrollY}px`;
+    this.alertBox.style.left = `${rect.left + window.scrollX}px`;
     this.alertBox.style.width = `${rect.width}px`;
     this.alertBox.style.height = `${rect.height}px`;
 
     container.addEventListener('wheel', (event) => {
       if (event.altKey) {
+        this.alertBox.style.display = 'none';
         map.scrollZoom.enable();
       } else {
         this.alertBox.style.display = 'flex';
       }
 
-      setTimeout(() => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
         this.alertBox.style.display = 'none';
         map.scrollZoom.disable();
       }, this.settings.timeout);
