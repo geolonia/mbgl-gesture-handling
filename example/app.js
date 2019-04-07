e1a4d0185b2f94ecf8e4293f48432af939b711da
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2019-04-08 00:55:34 +0900

    improve example

--- example/app.js
+++ example/app.js
@@ -21,25 +21,3 @@ map.addControl(new ForkMeConntrol({
 }));
 
 new GestureHandling().addTo(map);
-
-const map2 = new mapboxgl.Map({
-  container: 'map2',
-  style: 'https://tilecloud.github.io/tiny-tileserver/style.json',
-  attributionControl: true,
-  hash: true,
-  localIdeographFontFamily: "sans-serif",
-  interactive: true
-});
-
-map2.addControl(new mapboxgl.NavigationControl());
-map2.addControl(new mapboxgl.GeolocateControl());
-map2.addControl(new TileCloudControl());
-map2.addControl(new ForkMeConntrol({
-  url: 'https://github.com/tilecloud/mbgl-gesture-handling',
-}));
-
-new GestureHandling({
-  textMessage: "Altを押してね"
-}).addTo(map2);
-
-
