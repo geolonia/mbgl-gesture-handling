473f5ffe0d13349984d467d811dc0662794925e3
kamataryo <mugil.cephalus+Ciconia.local@gmail.com>
2020-03-05 23:35:38 +0900

    Update gesture handling

--- mbgl-gesture-handling.js
+++ mbgl-gesture-handling.js
@@ -1,119 +1,144 @@
 "use strict"
 
+function getLang() {
+  const lang = (
+    window.navigator.languages &&
+    window.navigator.languages[0] &&
+    window.navigator.languages[0].toLowerCase()
+  ) || window.navigator.language.toLowerCase()
+
+  if ('ja' === lang || 'ja-jp' === lang) {
+    return 'ja'
+  } else {
+    return 'en'
+  }
+}
+
 class GestureHandling {
-  constructor(options) {
-    this.fullscreen = false;
-    this.id = `mbgl-gesture-handling-help-container-${GestureHandling.count}`;
-    GestureHandling.count++;
-    this.timer = null;
+  constructor(options = {}) {
+    this.fullscreen = false
+    this.id = `mbgl-gesture-handling-help-container-${GestureHandling.count}`
+    GestureHandling.count++
+    this.timer = null
+
+    let textMessage = 'Use alt + scroll to zoom the map.'
+    let textMessageMobile = 'Use two fingers to move the map.'
+    const lang = (options.lang === 'auto' || !options.lang) ? getLang() : options.lang
+    if ('ja' === lang) {
+      textMessage = 'Alt キーを押しながらスクロールしてください。'
+      textMessageMobile = '2本指を使って操作してください。'
+    }
 
     this.settings = {
       backgroundColor: 'rgba(0, 0, 0, 0.8)',
       textColor: '#ffffff',
-      textMessage: 'Use alt + scroll to zoom the map.',
-      textMessageMobile: 'Use two fingers to move the map.',
+      textMessage,
+      textMessageMobile,
       timeout: 2000,
-      ...options
-    };
+      ...options,
+    }
 
-    this.helpElement = document.querySelector(`#${this.id}`);
+    this.helpElement = document.querySelector(`#${this.id}`)
 
     if (null === this.helpElement) {
-      this.helpElement = document.createElement('div');
-      this.helpElement.id = this.id;
-      this.helpElement.style.backgroundColor = this.settings.backgroundColor;
-      this.helpElement.style.position = 'absolute';
-      this.helpElement.style.display = 'none';
-      this.helpElement.style.zIndex = 9999;
-      this.helpElement.style.justifyContent = 'center';
-      this.helpElement.style.alignItems = 'center';
+      this.helpElement = document.createElement('div')
+      this.helpElement.id = this.id
+      this.helpElement.style.backgroundColor = this.settings.backgroundColor
+      this.helpElement.style.position = 'absolute'
+      this.helpElement.style.display = 'none'
+      this.helpElement.style.zIndex = 9999
+      this.helpElement.style.justifyContent = 'center'
+      this.helpElement.style.alignItems = 'center'
 
-      const textBox = document.createElement('div');
-      textBox.style.textAlign = 'center';
-      textBox.style.color = this.settings.textColor;
-      textBox.innerText = "";
+      const textBox = document.createElement('div')
+      textBox.style.textAlign = 'center'
+      textBox.style.color = this.settings.textColor
+      textBox.innerText = ''
 
-      this.helpElement.appendChild(textBox);
-      document.body.appendChild(this.helpElement);
+      this.helpElement.appendChild(textBox)
     }
   }
 
   showHelp(map, message) {
-    const rect = map.getContainer().getBoundingClientRect();
-    this.helpElement.style.top = `${rect.top + window.scrollY}px`;
-    this.helpElement.style.left = `${rect.left + window.scrollX}px`;
-    this.helpElement.style.width = `${rect.width}px`;
-    this.helpElement.style.height = `${rect.height}px`;
-    this.helpElement.style.display = 'flex';
+    this.helpElement.style.top = 0
+    this.helpElement.style.left = 0
+    this.helpElement.style.width = '100%'
+    this.helpElement.style.height = '100%'
+    this.helpElement.style.display = 'flex'
 
-    this.helpElement.querySelector('div').innerText = message;
+    this.helpElement.querySelector('div').innerText = message
+
+    map.getContainer().appendChild(this.helpElement)
   }
 
-  hideHelp() {
-    this.helpElement.style.display = 'none';
+  hideHelp(map) {
+    try {
+      map.getContainer().removeChild(this.helpElement)
+    } catch (e) {
+      // nothing to do
+    }
   }
 
   addTo(map) {
-    map.scrollZoom.disable();
+    map.scrollZoom.disable()
 
     this.helpElement.addEventListener('wheel', (event) => {
       if (event.altKey || true === this.fullscreen) {
         event.preventDefault()
-        this.hideHelp();
+        this.hideHelp(map)
       } else {
-        clearTimeout(this.timer);
+        clearTimeout(this.timer)
         this.timer = setTimeout(() => {
-          this.hideHelp();
-        }, this.settings.timeout);
+          this.hideHelp(map)
+        }, this.settings.timeout)
       }
-    });
+    })
 
     map.getContainer().addEventListener('wheel', (event) => {
       if (event.altKey || true === this.fullscreen) {
         event.preventDefault()
-        if (! map.scrollZoom.isEnabled()) {
-          map.scrollZoom.enable();
+        if (!map.scrollZoom.isEnabled()) {
+          map.scrollZoom.enable()
         }
       } else {
-        map.scrollZoom.disable();
-        this.showHelp(map, this.settings.textMessage);
+        map.scrollZoom.disable()
+        this.showHelp(map, this.settings.textMessage)
         this.timer = setTimeout(() => {
-          this.hideHelp();
-        }, this.settings.timeout);
+          this.hideHelp(map)
+        }, this.settings.timeout)
       }
-    });
+    })
 
     this.helpElement.addEventListener('touchstart', (event) => {
       if (event.touches && (2 <= event.touches.length || true === this.fullscreen)) {
-        clearTimeout(this.timer);
-        this.hideHelp();
-        map.dragPan.enable();
-        event.preventDefault();
+        clearTimeout(this.timer)
+        this.hideHelp(map)
+        map.dragPan.enable()
+        event.preventDefault()
       }
-    });
+    })
 
     map.on('movestart', (event) => {
       if (event.originalEvent && 'touches' in event.originalEvent
               && 2 > event.originalEvent.touches.length && false === this.fullscreen) {
-        this.showHelp(map, this.settings.textMessageMobile);
-        map.dragPan.disable();
+        this.showHelp(map, this.settings.textMessageMobile)
+        map.dragPan.disable()
         this.timer = setTimeout(() => {
-          map.dragPan.enable();
-          this.hideHelp();
-        }, this.settings.timeout);
+          map.dragPan.enable()
+          this.hideHelp(map)
+        }, this.settings.timeout)
       }
-    });
+    })
 
-    document.addEventListener("fullscreenchange", () => {
+    document.addEventListener('fullscreenchange', () => {
       if ( document.fullscreenElement ) {
-        this.fullscreen = true;
+        this.fullscreen = true
       } else {
-        this.fullscreen = false;
+        this.fullscreen = false
       }
-    });
+    })
   }
 }
 
 GestureHandling.count = 0 // static
-
 export default GestureHandling;
